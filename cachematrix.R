77499f500646c0a89392ed59613fb9987e795132
hwjupiter <jupitersdrops@gmail.com>
2017-12-10 09:18:53 +0200

    Test Commit
    dsfgsgh

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Cache a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv01 <- NULL   ##initialise
+  set01 <- function(y){    ##create "copies"
+    x <<- y
+    inv01 <<- NULL
+  }
+  get01 <- function() x
+  set_Inv01 <- function(solveMatrix)
+  inv01 <<- solveMatrix
+  get_Inv01 <- function() inv01
+  list(set01 = set01, get01 = get01, set_Inv01 = set_Inv01, get_Inv01 = get_Inv01) ## create the cached matrix
 }
 
 
-## Write a short comment describing this function
+## return the inverse of the matrix that was cached in the previous function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv01 <- x$get_Inv01()
+  if(!is.null(inv01)){
+    message("Retrieving Cache...")
+    return(inv01)
+  }
+  data02 <- x$get01()
+  inv01 <- solve(data02)
+  x$set_Inv01(inv01)
+  inv01  ##return the inverse  
 }
